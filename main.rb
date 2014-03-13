492d7256ac61e0f99c9b48962a03a4b271a19af4
murakamiginko <pusotsu@gmail.com>
2014-03-13 12:17:12 +0900

    returnじゃなくてtextに文字を代入するようにした

--- main.rb
+++ main.rb
@@ -21,17 +21,17 @@ end
 #update_name関数みたいなやつ
 def update_name(status)
 
-#statusに(@murakamiginko)があれば、そのテキストから(@murakamiginko)と@を削除する。
+  #statusに(@murakamiginko)があれば、そのテキストから(@murakamiginko)と@を削除する。
   if status.text.include?("(@murakamiginko)") then
     text = status.text.sub("(@murakamiginko)","")
     text = text.sub("@","")
 
-#名前が20文字を越えている場合、returnする
-  if text && 20 < text.length then
-   text = "20文字以内にしてください。"
-  end
-
-#名前にtextの中身を入れ、textとmurakamiginkoが等しい場合は"戻りました"、違う場合は"改名しました"をtweetに入れる。あとはその内容をツイートする。
+    #名前が20文字を越えている場合、
+    if text && 20 < text.length then
+      text = "20文字以内にしてください。"
+    end
+    
+    #名前にtextの中身を入れ、textとmurakamiginkoが等しい場合は"戻りました"、違う場合は"改名しました"をtweetに入れる。あとはその内容をツイートする。
     @rest_client.update_profile(:name => "#{text}")
     opt = {"in_reply_to_status_id"=>status.id.to_s}
     tweet = "murakamiginko" == text ? "@#{status.user.screen_name} 戻りました" : "@#{status.user.screen_name} #{text}に改名しました"
